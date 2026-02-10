da440114f3e8001dd14034389429cedea760f7b7
AaronTan11 <tanweihup@gmail.com>
2026-02-10 17:40:47 +0800

    initial commit

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,19 @@
+import { db } from "@my-superteam-fun/db";
+import * as schema from "@my-superteam-fun/db/schema/auth";
+import { env } from "@my-superteam-fun/env/server";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { nextCookies } from "better-auth/next-js";
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: "pg",
+
+    schema: schema,
+  }),
+  trustedOrigins: [env.CORS_ORIGIN],
+  emailAndPassword: {
+    enabled: true,
+  },
+  plugins: [nextCookies()],
+});
